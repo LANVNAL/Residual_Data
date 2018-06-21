1dfeeb3ecf5d842ed6f0d85221db7b7303b9d5fe
LANVNAL <503001939@qq.com>
2018-06-21 09:06:35 +0800

    Add files via upload

---
+++ Residual_data.py
@@ -0,0 +1,93 @@
+#-*- coding:utf-8 -*-
+
+from telegram import Bot
+from telegram.ext import Updater
+import logging
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import time
+import os
+import requests
+import re
+
+
+
+TOKEN = 'xxxxxx'
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+cookies = {
+    'PHPSESSID':'xxxxxxx',
+    'UM_distinctid':'xxxxx',
+    'CNZZDATA1260364996':'xxxx',
+    'Hm_lvt_e9ce8276ca2c4f210d70c15e8e788b63':'xxxxx',
+    'Hm_lpvt_e9ce8276ca2c4f210d70c15e8e788b63':'xxxxx'
+}
+header = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5)',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Referer':'http://kingss.vip/index.php/control/index/',
+    'Accept-Language':'zh-CN,zh;q=0.9'
+}
+
+url = 'http://kingss.vip/index.php/control/detail/xxxx/'
+
+def Inquire():
+    rqs = requests.get(url = url,headers = header,cookies = cookies)
+    #rqs.encoding='utf-8'
+    data = rqs.text
+    #data = unicode(data)
+    find = re.findall('(\d+.\d+) GB',data)
+    used_data = str(find[0])
+    remain_data = str(find[1])
+    return used_data,remain_data
+
+
+def command(handler,cmd=None,**kw):
+    def decorater(func):
+        def wrapper(*args,**kw):
+            return func(*args,**kw)
+        if cmd==None:
+            func_hander=handler(func,**kw)
+        else:
+            func_hander=handler(cmd,func,**kw)
+        dispatcher.add_handler(func_hander)
+        return wrapper
+    return decorater
+
+@command(CommandHandler,'start')
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="i'm a bot to inquire the residual data in kingss.Use /help to get help.")
+
+@command(CommandHandler,'help')
+def help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="use /data to inquire the Residual data.")
+
+@command(CommandHandler,'data')
+def data(bot, update):
+    used_data,remain_data = Inquire()
+    text = '已用流量:' + used_data + ' GB | 剩余流量:' + remain_data + ' GB'
+    bot.send_message(chat_id=update.message.chat_id, text=text)
+
+@command(MessageHandler,Filters.text)
+def echo(bot, update):
+	file=open('/root/1.txt','a')
+	file.write(update.message.text+'\n')
+	file.close()
+	bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+@command(CommandHandler,'caps',pass_args=True)
+def caps(bot,update,args):
+	text_caps=' '.join(args).upper()
+	bot.sendMessage(chat_id=update.message.chat_id,text=text_caps)
+
+updater.start_polling()
+
+while True:
+    time.sleep(1800)
+    Inquire()
+
+
+
+
